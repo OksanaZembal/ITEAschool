d3779661e477cbecb677e4712ebd6ddf5ebe8265
Oksana.Zembal <oksanazembal@gmail.com>
2019-08-15 21:32:41 +0300

    added new branch

--- lesson1/js/script2.js
+++ lesson1/js/script2.js
@@ -1,1 +1,19 @@
-console.log("hello my world");
+function j(a) {
+    if (a < 50 && a >= 20) {
+        return true;
+    }
+    return false;
+};
+console.log(j(35));
+const fn = (a) => a > 30 && a <= 20 ? true : false;
+
+// function fn(a) {
+
+// }
+
+function myF(a, b) {
+    a =
+
+
+};
+console.log(myF(3, 2));
